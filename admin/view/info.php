59789e3545cd0b329c76085de294094f88f58994
Luis-Enrique-Rodriguez <18032429@itcelaya.edu.mx>
2023-06-01 01:30:53 -0600

    modified:   admin/view/info.php 	modified:   admin/view/tratamiento.php 	new file:   images/bracket.jpg 	new file:   images/extraccion.jpg 	new file:   images/injerto.jpg 	new file:   images/limpieza.jpg 	new file:   images/mision.jpg 	new file:   images/que.jpg 	new file:   images/valores.jpg 	new file:   images/vision.jpg

--- admin/view/info.php
+++ admin/view/info.php
@@ -2,5 +2,77 @@
 include_once('return.php');  
 ?>
 
+<!-- Styles -->
+<link rel="stylesheet" href="../../css/style.css">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+
+
+<div class="row mx-auto w-40">
+<div class="card mb-3 ">
+  <div class="row g-0 ">
+    <div class="col-md-4 mt-4" style=" width: 70px;">
+      <img src="../../../clinica/images/que.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">¿Qué es FrissDent?</h5>
+        <p class="card-text">Somos una clinica especialidad en diversos contextos que tu boca lo requiera o tu cuerpo.
+            Atendemos desde lo mas facil hasta lo mas complejo en operaciones o cirugias que requieras
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="card mb-3 ">
+  <div class="row g-0 ">
+    <div class="col-md-4 mt-4" style=" width: 70px;">
+      <img src="../../../clinica/images/mision.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Misión</h5>
+        <p class="card-text">"Nuestra misión es brindar atención dental integral de alta calidad y excelencia, con un enfoque centrado en el paciente. Nos comprometemos a mejorar la salud oral y el bienestar general de nuestros pacientes, ofreciendo servicios odontológicos avanzados y personalizados. Buscamos establecer relaciones de confianza a largo plazo con nuestros pacientes, proporcionando un ambiente cálido, amigable y seguro. Nuestro equipo altamente capacitado y comprometido se esfuerza por ofrecer una atención ética, profesional y basada en evidencia científica, utilizando tecnología de vanguardia. Trabajamos con pasión y dedicación para lograr sonrisas saludables y felices en cada uno de nuestros pacientes.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+<div class="card mb-3 ">
+  <div class="row g-0 ">
+    <div class="col-md-4 mt-4" style="width: 70px;">
+      <img src="../../../clinica/images/vision.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Vision</h5>
+        <p class="card-text">Nos visualizamos como una clínica dental líder y de referencia en nuestra comunidad, reconocida por nuestra excelencia clínica, compromiso con la innovación y enfoque centrado en el paciente. Nos esforzamos por ser el destino preferido para el cuidado dental, brindando servicios de vanguardia y una experiencia excepcional. 
+    </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+<div class="card mb-3 ">
+  <div class="row g-0 ">
+    <div class="col-md-4 mt-4" style="width: 70px;">
+      <img src="../../../clinica/images/valores.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Valores</h5>
+        <p class="card-text">Excelencia, Integridad, Profesionalismo, Prevención
+    </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 
 
--- admin/view/tratamiento.php
+++ admin/view/tratamiento.php
@@ -2,3 +2,83 @@
 include_once('return.php');  
 ?>
 
+<!-- Styles -->
+<link rel="stylesheet" href="../../css/style.css">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+
+
+<div class="row mx-auto w-40">
+<div class="card mb-3 ">
+  <div class="row g-0 ">
+    <div class="col-md-4 mt-4" style=" width: 80px;">
+      <img src="../../../clinica/images/injerto.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Injerto</h5>
+        <p class="card-text">Cuando hay pérdida ósea significativa alrededor de los dientes
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="card mb-3 ">
+  <div class="row g-0 ">
+    <div class="col-md-4 mt-4" style=" width: 80px;">
+      <img src="../../../clinica/images/extraccion.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Extraccion de muelas</h5>
+        <p class="card-text">La extracción de muelas del juicio es un procedimiento quirúrgico para extraer una o más muelas del juicio —las cuatro piezas dentales permanentes de los adultos ubicadas en los extremos posteriores de la boca, tanto arriba como abajo—.
+
+Si una muela del juicio no tiene suficiente espacio para crecer (muela de juicio retenida) y, por lo tanto, provoca dolor, infección u otros problemas dentales, es probable que sea necesario extraerla. La extracción de muelas del juicio puede estar a cargo de un dentista o un cirujano bucal.
+
+Para prevenir problemas futuros, algunos dentistas y cirujanos bucales recomiendan extraer las muelas del juicio incluso cuando todavía no causan ningún problema.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+<div class="card mb-3 ">
+  <div class="row g-0 ">
+    <div class="col-md-4 mt-4" style="width: 80px;">
+      <img src="../../../clinica/images/bracket.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Brackets</h5>
+        <p class="card-text">La función de los brackets metálicos es la de mover los dientes a la posición funcional y estética correcta.
+Es decir, por un lado tienen como objetivo colocar los dientes para que el proceso de masticación sea el adecuado. 
+    </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+<div class="card mb-3 ">
+  <div class="row g-0 ">
+    <div class="col-md-4 mt-4" style="width: 80px;">
+      <img src="../../../clinica/images/limpieza.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Limpieza Dental</h5>
+        <p class="card-text">La limpieza dental, también llamada profilaxis dental, se hace para eliminar la placa y el cálculo (sarro) que se acumula en los dientes durante el paso del tiempo.
+
+Esta acumulación de placa y sarro se va depositando en los dientes. La placa es una sustancia viscosa con bacterias y restos de alimentos que se sitúa en los márgenes de la encía y en las zonas más retentivas de los dientes.  El sarro una vez se genera no se puede eliminar con el cepillado. El sarro es la solidificación de la placa dental.
+
+Con una correcta higiene oral la placa se elimina totalmente, para ello la correcta higiene se ha de hacer a diario, el cepillado es suficiente para ello.
+    </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
